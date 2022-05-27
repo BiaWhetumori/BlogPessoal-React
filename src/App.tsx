50addb9bc8ad1702e383fa01c27cdf5ba896722b
BiaWhetumori <beatrizantonia9@gmail.com>
2022-05-27 14:19:04 -0300

    Atividade React 4

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,24 @@ import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './pages/home/Home';
 import './App.css';
+import { Route, BrowserRouter as Router , Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
 
 
 function App() {
   return (
-   <>
-   <Navbar />
-   <Home  />
-   <Footer  />
-   </>
-  );
-}
-
-export default App;
+    <Router>
+      <Navbar />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cadastro" element={<CadastroUsuario />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+  )
+  }
+  export default App;
