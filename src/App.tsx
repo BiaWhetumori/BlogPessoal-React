b368a064d7645bb5a2165d30c40d8a0cd95dafaf
BiaWhetumori <beatrizantonia9@gmail.com>
2022-05-31 17:30:13 -0300

    implementar a segurança do seu projeto, fazendo o login e cadastro funcionarem corretamente

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
+import CadastroUsuario from './pages/cadastroUsuario/CadastroUsuario';
 import Home from './pages/home/Home';
 import './App.css';
 import { Route, BrowserRouter as Router , Routes } from 'react-router-dom';
@@ -16,7 +17,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/cadastro" element={<CadastroUsuario />} />
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} /> 
         </Routes>
       </div>
       <Footer />
