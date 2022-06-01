5c678d90d75c1376632e2db8968ff56d4bd99cd4
BiaWhetumori <beatrizantonia9@gmail.com>
2022-06-01 15:02:00 -0300

    Implementação do GetAll da model de Tema e Postagem

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import CadastroUsuario from './pages/cadastroUsuario/CadastroUsuario';
+import ListaTema from './components/temas/listatemas/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
 import Home from './pages/home/Home';
 import './App.css';
-import { Route, BrowserRouter as Router , Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Login from './pages/login/Login';
 
 
@@ -17,11 +19,13 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/cadastrousuario" element={<CadastroUsuario />} /> 
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+          <Route path="/temas" element={<ListaTema />} />
+          <Route path="/posts" element={<ListaPostagem />} />
         </Routes>
       </div>
       <Footer />
     </Router>
   )
-  }
-  export default App;
+}
+export default App;
